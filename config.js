105dfff9f7b56ae04e65ea3d2546745fcf7fa06a
Milan Dudhat <86527449+milandudhat@users.noreply.github.com>
2022-06-29 01:08:35 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose') 
-mongoose.connect('mongodb+srv://milandudhat:milan@cluster0.gjkd4ik.mongodb.net/auth?retryWrites=true&w=majority').then(()=>{
-    console.log("Database Connected");
-}).catch((err)=>{
-    console.log(err);
-})
